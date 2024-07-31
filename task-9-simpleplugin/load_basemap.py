49ddda3c65d60f825d91421dc6e519dd2bb75278
mulyungi <mulyungivic09@gmail.com>
2024-07-31 03:46:17 +0300

    updated simple plugin

--- task-9-simpleplugin/load_basemap.py
+++ task-9-simpleplugin/load_basemap.py
@@ -1,21 +1,24 @@
 '''
-the main file with all the functionality
+the main file with all the functionality adding the basemap
 where the main plugin class is written
-'''
+The PyQGIS code to load a XYZ tile layer 
+can be found here:'https://docs.qgis.org/3.34/en/docs/pyqgis_developer_cookbook/cheat_sheet.html'
+we will insert the layer at the bottom of the layers after loading the places.gpkg file
 
+'''
 import os
 import inspect
 from PyQt5.QtWidgets import QAction
 from PyQt5.QtGui import QIcon
+from qgis.core import QgsRasterLayer, QgsProject
 
 cmd_folder = os.path.split(inspect.getfile(inspect.currentframe()))[0]
 
-#plugin manager calls this class to create an instance of the plugin
+#access iface via self.iface
 class BasemapLoaderPlugin:
-    #access iface is using self.iface
     def __init__(self, iface):
         self.iface = iface
-    
+    #create the button with icon + add to plugins toolbar
     def initGui(self):
         icon = os.path.join(os.path.join(cmd_folder, 'icons8-python-48.png'))
         self.action = QAction(QIcon(icon), 'Load Basemap', self.iface.mainWindow())
@@ -25,7 +28,24 @@ class BasemapLoaderPlugin:
     def unload(self):
         self.iface.removeToolBarIcon(self.action)
         del self.action
-        
+    #where all the magic happens--data processing goes here
     def run(self):
-        #where the magic happens. some processing is done here
-        self.iface.messageBar().pushMessage('HOORAY! This is your first plugin')
+        #url for tile layer we want
+        basemap_url = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+        zmin = 0
+        zmax = 19
+        crs = 'EPSG:3857'
+        #calls url
+        uri = f'type=xyz&url={basemap_url}&zmax={zmax}&zmin={zmin}$crs={crs}'
+        #create raster layer
+        rlayer = QgsRasterLayer(uri, 'OpenStreetMap', 'wms')
+        if rlayer.isValid():
+            # Add the layer, but not to the legend
+            QgsProject.instance().addMapLayer(rlayer, False)
+            # Insert layer at the bottom of Layer Tree
+            root = QgsProject.instance().layerTreeRoot()
+            position = len(root.children())
+            root.insertLayer(position, rlayer)
+            self.iface.messageBar().pushSuccess('Success', 'Basemap Layer Loaded')
+        else:
+            self.iface.messageBar().pushCritical('Error', 'Invalid Basemap Layer')
