4c356c2aea322f483750bc7af8bcbdc025247258
mulyung1 <victorsonofmartin@gmail.com>
2024-08-19 00:19:51 +0300

    task 12 done!

--- 12.2-task_Selecting_Neighbors.py
+++ 12.2-task_Selecting_Neighbors.py
@@ -1,23 +1,25 @@
-#access the ids of active layer
-layer_id='[%layer_id%]'
-field_id=[%$id%]
+#inject the layer id and feature id into script
+layer_id = '[%@layer_id%]'
+fid = [% $id %]
 
-#make a layer object of specified layer(right-clicked layer) 
-layer=QgsProject.instance().mapLayer(layer_id)
+#retrieve layer object corresponding to given layer id 
+layer = QgsProject.instance().mapLayer(layer_id)
 
-def get_neighbors(field_id):
-    #get our iterator
-    feat=layer.getFeature(field_id)
+def get_neighbors(fid):
+    f = layer.getFeature(fid)
     '''to get all intersecting features >>use list comprehension 
-       use touches() mtd = if data is topologically correct. 
-       supply the bounding box to iterator(getFeatures())to use spatial index
+      use touches() mtd = if data is topologically correct. 
+      supply the bounding box to iterator(getFeatures())to use spatial index
     '''
-    neighbors_list=[x.id() for x in layer.getFeatures(feat.geometry().boundingBox()) if x.geometry().intersects(feat.geometry()) and x.id() != feat.id()]
-    return neighbors_list
+    neighbors = [
+        x.id()
+        for x in layer.getFeatures(f.geometry().boundingBox())
+        if x.geometry().intersects(f.geometry()) and x.id() != f.id()
+    ]
+    return neighbors
+
 #call the method and store it in a variable
-first_degree_neighbors=get_neighbors(field_id)
+first_degree_neighbors = get_neighbors(fid)
 
-#apply the selection
+# Apply the selection
 layer.selectByIds(first_degree_neighbors)
-
-
